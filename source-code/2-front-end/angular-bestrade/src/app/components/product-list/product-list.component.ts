69aebb028bd98da3719ed1a70d6b15f5e33c49a4
Aman Rauniyar <amanrauniyar@outlook.com>
2022-06-30 22:34:29 -0400

    Updated Angular Component
    To subscribe to data from Angular service by unwrapping the JSON and display the data in an HTML page.

--- source-code/2-front-end/angular-bestrade/src/app/components/product-list/product-list.component.ts
+++ source-code/2-front-end/angular-bestrade/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/common/product';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  products: Product[];
+    
+  constructor(private productService: ProductService) { } /* Inject the dependency
+  ProductService into this product list component. */
 
-  ngOnInit(): void {
+  /* Once the angular component has been initialized they'll call the ngOnInIt mwthod. */
+  ngOnInit() {
+  /* Similar to @PostConstruct where hook is added to call my listProducts method. */
+  this.listProducts() 
+  }
+
+  listProducts() {
+    /* Make use of my productService and call getProductList and actually subscribe to the
+    data. Method is invoked once you subsribe() in an asynchronus fashion.*/ 
+    this.productService.getProductList().subscribe(
+      data => {
+      /* Assign results to the Product array for integrating our service with Angular 
+      Component */
+        this.products = data;
+      }
+    )
   }
 
 }
--- source-code/2-front-end/angular-bestrade/src/app/services/product.service.ts
+++ source-code/2-front-end/angular-bestrade/src/app/services/product.service.ts
@@ -6,28 +6,30 @@ import { map, Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService { /* This class will make a get request to that back-end
-base URL, grab the data unwrap it accordingly and make it available as an array of
-products. */ 
-
+ /* The class below will make a get request to that back-end base URL, grab the data 
+ unwrap it accordingly and make it available as an array of products. */ 
+ export class ProductService {
   private baseUrl = 'http://localhost:8080/api/products'; // Connect to product API
 
-  constructor(private httpClient: HttpClient) { } /* Injecting HttpClient as Angular 
-  has a dependence injection framework */ 
-
+ /* Injecting HttpClient as Angular has a dependence injection framework */ 
+  constructor(private httpClient: HttpClient) { }
   // A method to get Product List which returns an observable of product array
-  getProductList(): Observable<Product[]>{ /* Returns an observable: Map the JSON data 
-  from Spring Data REST to Product Array */
+  getProductList(): Observable<Product[]>{ 
+  /* Returns an observable: Map the JSON data from Spring Data REST to Product Array */
     return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
-      map(response => response._embedded.products) /* Map the data to our given data type
-      to grab that response from embedded: products to make use of products array*/ 
+  /* Map the data to our given data type to grab that response from embedded: products 
+  to make use of products array*/ 
+    map(response => response._embedded.products)
     ); 
   } 
 }
-interface GetResponse { /* Also adding an supporting interface to help us with the
-mapping above to unwrap the JSON data from the spring of data REST API */
-  _embedded: { /* This will help us to make use of _embedded entry that comes back from
-   the spring data REST API. */
-    products: Product[]; // Acess the array of product
+ /* Also adding an supporting interface to help us with the mapping above to unwrap the 
+ JSON data from the spring of data REST API */
+ interface GetResponse {
+ /* This will help us to make use of _embedded entry that comes back from the spring 
+ data REST API. */
+   _embedded: {
+    // Acess the array of product
+    products: Product[];
   }
 }
