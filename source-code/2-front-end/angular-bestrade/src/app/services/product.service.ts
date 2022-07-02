901f7f8f8fd2de0d1a777b88c7e310c2eba6ec19
Aman Rauniyar <amanrauniyar@outlook.com>
2022-07-02 18:28:31 -0400

    Call Product Service (Dynamic Search Component)
    Fixed the error from previous commit for the product service which was due to undeclared method was solved after doing both steps below.
    
    Enhanced menu component to read data from product service. Did it by defining the productCategories array in the product service, injecting the service in the constructor, defining and calling list product categories.
    
    Updated product service to call URL on Spring Boot app by creating an interface, and method for get response from product category like products.

--- source-code/2-front-end/angular-bestrade/src/app/services/product.service.ts
+++ source-code/2-front-end/angular-bestrade/src/app/services/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Product } from '../common/product';
 import { map, Observable } from 'rxjs';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,10 @@ import { map, Observable } from 'rxjs';
  Spring Data REST returns only 20 in first page but, can do a workaround by adding
  "?size=100" at the end to list 100 in the first page. */ 
  export class ProductService {
-  private baseUrl = 'http://localhost:8080/api/products'; // Connect to product API
 
+  private baseUrl = 'http://localhost:8080/api/products'; // Connect to product API
+  // Connect to product category API
+  private categoryUrl = 'http://localhost:8080/api/products/category';
  /* Injecting HttpClient as Angular has a dependence injection framework */ 
   constructor(private httpClient: HttpClient) { }
   // A method to get Product List which returns an observable of product array
@@ -23,16 +26,28 @@ import { map, Observable } from 'rxjs';
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
   /* Returns an observable: Map the JSON data from Spring Data REST to Product Array */
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
   /* Map the data to our given data type to grab that response from embedded: products 
   to make use of products array*/ 
     map(response => response._embedded.products)
     ); 
   } 
+  /* A method to get Product Categories which returns an observable of product 
+  categories array */
+  getProductCategories(): Observable <ProductCategory[]> {
+    /* Returns an observable: Map the JSON data from Spring Data REST to Product 
+    Category Array */
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+       /* Map the data to our given data type to grab that response from embedded: products 
+       to make use of product category array*/ 
+      map(response => response._embedded.productCategory)
+    ); 
+  }
+
 }
  /* Also adding an supporting interface to help us with the mapping above to unwrap the 
  JSON data from the spring of data REST API */
- interface GetResponse {
+ interface GetResponseProducts {
  /* This will help us to make use of _embedded entry that comes back from the spring 
  data REST API. */
    _embedded: {
@@ -40,3 +55,12 @@ import { map, Observable } from 'rxjs';
     products: Product[];
   }
 }
+
+interface GetResponseProductCategory {
+  /* This will help us to make use of _embedded entry that comes back from the
+  spring data REST API. */
+    _embedded: {
+     // Acess the array of product
+     productCategory: ProductCategory[];
+   }
+ }
