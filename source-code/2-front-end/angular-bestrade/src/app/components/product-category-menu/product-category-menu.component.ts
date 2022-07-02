b18c6d3437f39136ce46904f69a7c7b185b909ff
Aman Rauniyar <amanrauniyar@outlook.com>
2022-07-02 19:23:57 -0400

    View Console Logs in Browser (Dynamic Search Component)
    Did this in chrome by selecting the menu bar>more tools>developer tools which brings up the console and the log of the website. In my case, it was product categories which was stringified the data into JSON in product-category-menu.component.ts file.

--- source-code/2-front-end/angular-bestrade/src/app/components/product-category-menu/product-category-menu.component.ts
+++ source-code/2-front-end/angular-bestrade/src/app/components/product-category-menu/product-category-menu.component.ts
@@ -5,29 +5,25 @@ import { ProductService } from 'src/app/services/product.service';
 @Component({
   selector: 'app-product-category-menu',
   templateUrl: './product-category-menu.component.html',
-  styleUrls: ['./product-category-menu.component.css']
+  styleUrls: ['./product-category-menu.component.css'],
 })
+
 export class ProductCategoryMenuComponent implements OnInit {
-  
   // Define the product categories property by an array of product category.
   productCategories: ProductCategory[];
-  
-  // Inject the service in the constructor.
-  constructor(private productService: ProductService) { }
 
+  // Inject the service in the constructor.
+  constructor(private productService: ProductService) {}
+
+  // Call listProductCategories method
   ngOnInit() {
     this.listProductCategories();
   }
 
   listProductCategories() {
-
-    this.productService.getProductCategories().subscribe(
-      data => {
-        console.log('Product Categories=' + JSON.stringify(data));
-        this.productCategories = data;
-      }
-    );
-    
+    this.productService.getProductCategories().subscribe((data) => {
+      console.log('Product Categories=' + JSON.stringify(data));
+      this.productCategories = data;
+    });
   }
-
 }
