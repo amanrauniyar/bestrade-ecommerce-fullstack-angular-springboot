c9ff10852ddf61f27ad1956151bad5ab4e952e5d
Aman Rauniyar <amanrauniyar@outlook.com>
2022-07-02 18:05:16 -0400

    Create Component to implement the (Dynamic Search Component)
    Generated product-category inside common and added two properties id and category name in the class product-category.ts
    
    Created a new component for the product category menu. Added some code to ts file by defining the property , injecting the service in constructor and defining a new method to list all products. (it has some issues, will fix soon)

---
+++ source-code/2-front-end/angular-bestrade/src/app/components/product-category-menu/product-category-menu.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductCategory } from 'src/app/common/product-category';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product-category-menu',
+  templateUrl: './product-category-menu.component.html',
+  styleUrls: ['./product-category-menu.component.css']
+})
+export class ProductCategoryMenuComponent implements OnInit {
+  
+  // Define the product categories property by an array of product category.
+  productCategories: ProductCategory[];
+  
+  // Inject the service in the constructor.
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+    this.listProductCategories();
+  }
+
+  listProductCategories() {
+
+    this.productService.getProductCategories().subscribe(
+      data => {
+        console.log('Product Categories=' + JSON.stringify(data));
+        this.productCategories = data;
+      }
+    );
+    
+  }
+
+}
