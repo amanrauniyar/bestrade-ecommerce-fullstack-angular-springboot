4ab9e17516ea22375eae7dd17e26f1ce2a58ca68
Aman Rauniyar <amanrauniyar@outlook.com>
2022-07-02 17:39:45 -0400

    Search for Products by Category (Dynamic Search Component)
    Changing the coding of category names and IDs from static/hardcoded to dynamic. Enhancing the app to read categories from database via REST API.
    
    First step off this process is by modifying Spring Boot app by exposing entity ids as there was no entity id at the product category level in the REST API.

--- source-code/1-back-end/spring-boot-bestrade/src/main/java/org/perscholas/amanrauniyar/bestrade/config/MyDataRestConfig.java
+++ source-code/1-back-end/spring-boot-bestrade/src/main/java/org/perscholas/amanrauniyar/bestrade/config/MyDataRestConfig.java
@@ -2,15 +2,29 @@ package org.perscholas.amanrauniyar.bestrade.config;
 
 import org.perscholas.amanrauniyar.bestrade.entity.Product;
 import org.perscholas.amanrauniyar.bestrade.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration /* Adding support for the configuration class with annotation, basically allowing it to scan so, Spring
 would pick this up. */
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager; // Autowire JPA entity manager
+
+    @Autowired // Optional as in this case, there is only one constructor, but done to be explicit.
+    public MyDataRestConfig(EntityManager theEntMgr) {
+        entityManager = theEntMgr;
+    }
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors); /* Generated method
@@ -34,6 +48,38 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         /* The above block of code will disable the HTTP methods for the Product Repository Category methods PUT, POST &
         DELETE. So, only GET will work, effectively making this read-only. */
 
+        // Call an internal helper method
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+
+        // Expose entity ids
+
+        // - Get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // - Create an array of the entity types
+        List<Class> entityClasses = new ArrayList<>();
+
+        // - Get the entity types for the entities
+        for (EntityType tempEntityType : entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        // - Expose the entity ids for the array of entity/domain types
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 }
+
+
+
+
+
+
+
+
+
+
 
