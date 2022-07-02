16891defbd35dde7956c3953e4b623148c234051
Aman Rauniyar <amanrauniyar@outlook.com>
2022-07-02 15:26:18 -0400

    Search for Products by Category
    Define routes and configure it based on the routes. Created a comma-delimited list of route items by specifying the path and the component in app.module.ts, if it matches then it will create new instance of component.

--- source-code/2-front-end/angular-bestrade/src/app/app.module.ts
+++ source-code/2-front-end/angular-bestrade/src/app/app.module.ts
@@ -6,12 +6,23 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 
+import { Routes, RouterModule} from '@angular/router'
+// Order of routes is important. First match wins. Start from specific to generic.
+const routes: Routes = [
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent}, // Set up routes for category by itself
+  {path: 'products', component: ProductListComponent}, // Also set up routes for products by itself
+  {path: '', redirectTo: '/products', pathMatch: 'full'}, // Empty paths will redirect to /products where rules don't apply so, forward slash is used to match full path instead of prefix.
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}, // Generic wild card (**) will redirect to /products if they don't match on anything on above routes.
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule
   ],
