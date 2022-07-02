52f56de427941db3033527ad48e0ca1f5715b8cd
Aman Rauniyar <amanrauniyar@outlook.com>
2022-07-02 15:44:29 -0400

    Define the Router Outlet and Set up Router links to pass category id param
    Defined the routes in app.module.ts
    
    Set up Route links to app.component.html in the menu bar by passing the category followed by a back slash and param name which is "1" for the books and so on foor the menu links based on the Routes configuration where we are passing an id to the category to set it up. At this moment, hard-coded the categories but later will make it dynamic and read the information from REST API. But for now, let's get it working with basics.
    
    Commented out the Mouse Pads and Luggage tags for now,

--- source-code/2-front-end/angular-bestrade/src/app/app.module.ts
+++ source-code/2-front-end/angular-bestrade/src/app/app.module.ts
@@ -6,27 +6,28 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 
-import { Routes, RouterModule} from '@angular/router'
-// Order of routes is important. First match wins. Start from specific to generic.
+import { Routes, RouterModule } from '@angular/router';
+/* Define routes. Order of routes is important. First match wins. Start from specific to 
+generic. */
 const routes: Routes = [
-  {path: 'category/:id', component: ProductListComponent},
-  {path: 'category', component: ProductListComponent}, // Set up routes for category by itself
-  {path: 'products', component: ProductListComponent}, // Also set up routes for products by itself
-  {path: '', redirectTo: '/products', pathMatch: 'full'}, // Empty paths will redirect to /products where rules don't apply so, forward slash is used to match full path instead of prefix.
-  {path: '**', redirectTo: '/products', pathMatch: 'full'}, // Generic wild card (**) will redirect to /products if they don't match on anything on above routes.
+  // Set up routes for category by id
+  { path: 'category/:id', component: ProductListComponent },
+  // Set up routes for category by itself
+  { path: 'category', component: ProductListComponent }, 
+  // Also set up routes for products by itself
+  { path: 'products', component: ProductListComponent }, 
+  /* Empty paths will redirect to /products where rules don't apply so, forward slash is used 
+  to match full path instead of prefix. */
+  { path: '', redirectTo: '/products', pathMatch: 'full' }, 
+  /* Generic wild card (**) will redirect to /products if they don't match on anything on 
+  above routes. */
+  { path: '**', redirectTo: '/products', pathMatch: 'full' }, 
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent
-  ],
-  imports: [
-    RouterModule.forRoot(routes),
-    BrowserModule,
-    HttpClientModule
-  ],
+  declarations: [AppComponent, ProductListComponent],
+  imports: [RouterModule.forRoot(routes), BrowserModule, HttpClientModule],
   providers: [ProductService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
