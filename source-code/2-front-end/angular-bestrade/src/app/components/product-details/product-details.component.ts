5bb00fbc85f99518b3b6d17b1f3f1525dd32b9d8
Aman Rauniyar <amanrauniyar@outlook.com>
2022-07-02 23:57:32 -0400

    Enhance the Product Details Component
    Added product property, injected dependency for actual ProductService and ActivatedRoute, Subscribe on the params and make a call to method handleProductDetails by defining the method.
    
    There is still errors on the product service but will fix it later.

--- source-code/2-front-end/angular-bestrade/src/app/components/product-details/product-details.component.ts
+++ source-code/2-front-end/angular-bestrade/src/app/components/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  product: Product = new Product(); // Define a property for product
+
+  // Injecting the dependency for actual ProductService and ActivatedRoute.
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // Subscribe on the params and make a call to method handleProductDetails. 
+    this.route.paramMap.subscribe(() => {
+      this.handleProductDetails();
+    })
+  }
+
+  // Define the method handleProductDetails
+  handleProductDetails() {
+
+    // Get the "id" param string. convert string to a number using the "+" symbol
+    const theProductId: number = +this.route.snapshot.paramMap.get('id')!;
+
+    this.productService.getProduct(theProductId).subscribe(
+      data => {
+        this.product = data;
+      }
+    )
   }
 
 }
