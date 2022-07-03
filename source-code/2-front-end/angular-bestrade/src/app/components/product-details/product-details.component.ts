7880dff980afda76cf97f8fa382e4e913b9c2be4
Aman Rauniyar <amanrauniyar@outlook.com>
2022-07-03 18:19:58 -0400

    Add to Cart from the Details View page of product
    Added functionality of cart code in product-details.component.html using button which calls addToCart() whose logic is implemented in product-details.component.ts file.

--- source-code/2-front-end/angular-bestrade/src/app/components/product-details/product-details.component.ts
+++ source-code/2-front-end/angular-bestrade/src/app/components/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
+import { CartItem } from 'src/app/common/cart-item';
 
 @Component({
   selector: 'app-product-details',
@@ -13,7 +15,7 @@ export class ProductDetailsComponent implements OnInit {
   product: Product = new Product(); // Define a property for product
 
   // Injecting the dependency for actual ProductService and ActivatedRoute.
-  constructor(private productService: ProductService,
+  constructor(private productService: ProductService, private cartService: CartService,
               private route: ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -34,6 +36,12 @@ export class ProductDetailsComponent implements OnInit {
         this.product = data;
       }
     )
+  }
+
+  addToCart() {
+    console.log(`Adding to cart: ${this.product.name}, ${this.product.unitPrice}`);
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem);
   }
 
 }
