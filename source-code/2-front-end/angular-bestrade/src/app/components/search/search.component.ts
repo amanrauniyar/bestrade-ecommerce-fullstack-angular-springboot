5da715fbe551d32227992e7ea4c8b874a1dea4c5
Aman Rauniyar <amanrauniyar@outlook.com>
2022-07-02 22:27:39 -0400

    Update ProductService API and implemented the search by keyword
    Update ProductService to call URL on Spring Boot app. Added searchProducts methods inside product.service.ts to have a method signature which can search by the keyword and render the result from REST API to server/client page.

--- source-code/2-front-end/angular-bestrade/src/app/components/search/search.component.ts
+++ source-code/2-front-end/angular-bestrade/src/app/components/search/search.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
+
 export class SearchComponent implements OnInit {
   
   // Inject the router into the component
--- source-code/2-front-end/angular-bestrade/src/app/services/product.service.ts
+++ source-code/2-front-end/angular-bestrade/src/app/services/product.service.ts
@@ -7,17 +7,20 @@ import { ProductCategory } from '../common/product-category';
 @Injectable({
   providedIn: 'root'
 })
+
+ export class ProductService {
+
  /* The class below will make a get request to that back-end base URL, grab the data 
  unwrap it accordingly and make it available as an array of products. By, default 
  Spring Data REST returns only 20 in first page but, can do a workaround by adding
  "?size=100" at the end to list 100 in the first page. */ 
- export class ProductService {
-
   private baseUrl = 'http://localhost:8080/api/products'; // Connect to product API
+  
   // Connect to product category API
   private categoryUrl = 'http://localhost:8080/api/product-category';
  /* Injecting HttpClient as Angular has a dependence injection framework */ 
   constructor(private httpClient: HttpClient) { }
+
   // A method to get Product List which returns an observable of product array
   getProductList(theCategoryId: number): Observable<Product[]>{ 
 
@@ -25,13 +28,10 @@ import { ProductCategory } from '../common/product-category';
     exposes endpoint so will need to make only the base URL dynamic. */
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
-  /* Returns an observable: Map the JSON data from Spring Data REST to Product Array */
-    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
-  /* Map the data to our given data type to grab that response from embedded: products 
-  to make use of products array*/ 
-    map(response => response._embedded.products)
-    ); 
+    return this.getProducts(searchUrl);  
+
   } 
+
   /* A method to get Product Categories which returns an observable of product 
   categories array */
   getProductCategories(): Observable <ProductCategory[]> {
@@ -42,6 +42,26 @@ import { ProductCategory } from '../common/product-category';
        to make use of product category array*/ 
       map(response => response._embedded.productCategory)
     ); 
+  }
+
+  /* Line 56 calls the method getProducts on lines 59-65 and returns the value to function
+  searchProducts which was just extracted in line 50-57. */
+
+  searchProducts(theKeyword: string): Observable<Product[]> {
+  /* I need to build URL based on the category id. Spring Data REST automatically 
+  exposes endpoint so will need to make only the base URL dynamic. */
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+
+  /* Returns an observable: Map the JSON data from Spring Data REST to Product Array */
+    return this.getProducts(searchUrl); 
+  }
+
+  private getProducts(searchUrl: string): Observable<Product[]> {
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
+      /* Map the data to our given data type to grab that response from embedded: products
+      to make use of products array*/
+      map(response => response._embedded.products)
+    );
   }
 
 }
