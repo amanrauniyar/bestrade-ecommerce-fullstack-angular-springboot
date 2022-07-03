89400c82c0a3e1c6f5395bd1d100c6c4dd8fa1f2
Aman Rauniyar <amanrauniyar@outlook.com>
2022-07-02 20:50:18 -0400

    Send data to search
    Updated search-component.html to have a new div with a search box and logic nowSearch method to do it with the user's value.
    
    Also updated search.component.ts to inject the router into the component, getting the logs of values for debugging if needed and calling the route search with the given keyword handled by the ProductListComponent to reuse the logic and view for listing products.

--- source-code/2-front-end/angular-bestrade/src/app/components/search/search.component.ts
+++ source-code/2-front-end/angular-bestrade/src/app/components/search/search.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
+  
+  // Inject the router into the component
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  nowSearch(value: string) {
+    // Getting the logs of values for debugging purposes
+    console.log(`value = ${value}`);
+    /* Call the route search with the given keyword and routing the data to search route
+    which will be handled by the ProductListComponent to reuse the logic and view for 
+    listing products. */ 
+    this.router.navigateByUrl(`/search/${value}`);
   }
 
 }
