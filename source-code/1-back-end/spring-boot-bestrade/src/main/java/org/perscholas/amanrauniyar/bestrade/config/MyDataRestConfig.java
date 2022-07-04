d53985215beffd11d45926a028301b61f6ec340f
Aman Rauniyar <amanrauniyar@outlook.com>
2022-07-04 11:07:54 -0400

    Checkout Form -  Populate Countries and States - REST Config
    Final Backend phase - Updated Spring Data REST Configs to make sure that the REST APIs in place are read-only to make sure no modifications are provided to countries or states via REST APIs.

--- source-code/1-back-end/spring-boot-bestrade/src/main/java/org/perscholas/amanrauniyar/bestrade/config/MyDataRestConfig.java
+++ source-code/1-back-end/spring-boot-bestrade/src/main/java/org/perscholas/amanrauniyar/bestrade/config/MyDataRestConfig.java
@@ -1,7 +1,9 @@
 package org.perscholas.amanrauniyar.bestrade.config;
 
+import org.perscholas.amanrauniyar.bestrade.entity.Country;
 import org.perscholas.amanrauniyar.bestrade.entity.Product;
 import org.perscholas.amanrauniyar.bestrade.entity.ProductCategory;
+import org.perscholas.amanrauniyar.bestrade.entity.State;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -15,11 +17,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-@Configuration /* Adding support for the configuration class with annotation, basically allowing it to scan so, Spring
-would pick this up. */
-public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Configuration /* Adding support for the configuration class with annotation, basically allowing it to scan so, Spring
+    would pick this up. */
+    public class MyDataRestConfig implements RepositoryRestConfigurer {
 
     private EntityManager entityManager; // Autowire JPA entity manager
+
 
     @Autowired // Optional as in this case, there is only one constructor, but done to be explicit.
     public MyDataRestConfig(EntityManager theEntMgr) {
@@ -28,28 +31,37 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors); /* Generated method
-        automatically by using the feature implement methods to configure repository REST configuration.*/
+        //automatically by using the feature implement methods to configure repository REST configuration. */
 
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
 
         // Disable HTTP methods for ProductRepository: PUT, POST & DELETE
         config.getExposureConfiguration()
                 .forDomainType(Product.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
         /* The above block of code will disable the HTTP methods for the Product Repository methods PUT, POST & DELETE.
         So, only GET will work, effectively making this read-only. */
 
         // Disable HTTP methods for ProductRepositoryCategory: PUT, POST & DELETE
-        config.getExposureConfiguration()
-                .forDomainType(ProductCategory.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+        disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
         /* The above block of code will disable the HTTP methods for the Product Repository Category methods PUT, POST &
         DELETE. So, only GET will work, effectively making this read-only. */
+        disableHttpMethods(Product.class, config, theUnsupportedActions);
+        disableHttpMethods(Country.class, config, theUnsupportedActions);
+        disableHttpMethods(State.class, config, theUnsupportedActions);
 
         // Call an internal helper method
         exposeIds(config);
+    }
+
+    private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[]
+                                    theUnsupportedActions) { config.getExposureConfiguration()
+                                    .forDomainType(theClass)
+                                    .withItemExposure((metdata, httpMethods) -> httpMethods.disable
+                                            (theUnsupportedActions))
+                                    .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable
+                                            (theUnsupportedActions));
     }
 
     private void exposeIds(RepositoryRestConfiguration config) {
--- source-code/1-back-end/spring-boot-bestrade/src/test/java/org/perscholas/amanrauniyar/bestrade/SpringBootBestradeApplicationTests.java
+++ source-code/1-back-end/spring-boot-bestrade/src/test/java/org/perscholas/amanrauniyar/bestrade/SpringBootBestradeApplicationTests.java
@@ -3,6 +3,7 @@ package org.perscholas.amanrauniyar.bestrade;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class SpringBootBesTradeApplicationTests {
 
