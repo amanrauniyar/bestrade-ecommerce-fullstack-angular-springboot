66faa81d8e351da9eeb37753079481f9f573d1a4
Aman Rauniyar <amanrauniyar@outlook.com>
2022-07-04 03:36:46 -0400

    Checkout Form -  Populate Countries and States - Repositories 2
    In the dao package, created the Spring Data REST repository for state entity as an interface.  Now, have REST API for retrieving countries.
    
    Also with the query method, now user can select state from the narrowed drop-down list at it filtered and narrowed down for a specific country code.

---
+++ source-code/1-back-end/spring-boot-bestrade/src/main/java/org/perscholas/amanrauniyar/bestrade/dao/StateRepository.java
@@ -0,0 +1,19 @@
+package org.perscholas.amanrauniyar.bestrade.dao;
+
+import org.perscholas.amanrauniyar.bestrade.entity.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
+
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource
+public interface StateRepository extends JpaRepository<State, Integer> {
+
+    // Special Finder/Query method for finding states by the country code
+    List<State> findByCountryCode(@Param("code") String code); /* To retrieve states for a specific country code very
+    useful for the user as they would select the country first and then states as it narrows down. */
+
+}
