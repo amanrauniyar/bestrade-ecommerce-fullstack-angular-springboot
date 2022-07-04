da4ca4837bc99ca969de8340985e2620abfce9c8
Aman Rauniyar <amanrauniyar@outlook.com>
2022-07-04 03:12:57 -0400

    Develop JPA Entities (Country, State)
    Wrote the code for JPA Entities for countries and state by setting up the basic fields and also setting up the relationships of one-to-many and many-to-one.

---
+++ source-code/1-back-end/spring-boot-bestrade/src/main/java/org/perscholas/amanrauniyar/bestrade/entity/Country.java
@@ -0,0 +1,32 @@
+package org.perscholas.amanrauniyar.bestrade.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="country")
+@Getter
+@Setter
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="code")
+    private String code;
+
+    @Column(name="name")
+    private String name;
+
+    // Set up one-to-many because one country has many states
+    @OneToMany(mappedBy = "country")
+    @JsonIgnore
+    private List<State> states;
+
+}
---
+++ source-code/1-back-end/spring-boot-bestrade/src/main/java/org/perscholas/amanrauniyar/bestrade/entity/State.java
@@ -0,0 +1,27 @@
+package org.perscholas.amanrauniyar.bestrade.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="state")
+@Data
+public class State {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="name")
+    private String name;
+
+    // Set up many-to-one because many states belongs to one country
+    @ManyToOne
+    /* Join on country_id in the state table so country id maps back to actual country table as a foreign key
+    relationship. */
+    @JoinColumn(name="country_id")
+    private Country country;
+
+}
