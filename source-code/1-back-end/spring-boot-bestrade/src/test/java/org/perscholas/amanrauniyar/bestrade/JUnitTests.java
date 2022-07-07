af320f22f2b0c5f251ed1c58d3f7e37f77ad6034
Aman Rauniyar <amanrauniyar@outlook.com>
2022-07-07 11:39:47 -0400

    Update JUnit Tests
    Added some more tests so that users can see both positive and negative results at once instead of going back and commenting it out.

--- source-code/1-back-end/spring-boot-bestrade/src/test/java/org/perscholas/amanrauniyar/bestrade/JUnitTests.java
+++ source-code/1-back-end/spring-boot-bestrade/src/test/java/org/perscholas/amanrauniyar/bestrade/JUnitTests.java
@@ -9,32 +9,60 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest()
 public class JUnitTests {
 
-	@Test
-	void testPositiveUserEmail() {
+	@Test // Positive Test Result Expected
+	void posTestCorrectUserEmail() {
 		User u = new User(2, "xyz@gmail.com", "xyz", "xyz");
 		assertNotNull(u);
 		assertEquals("xyz@gmail.com", u.getEmailId());
 	}
 
-	@Test
-	void testNegativeUserEmail() {
-		User u = new User(2, "poonam@gmail.com", "xyz", "xyz");
+	@Test // Positive Test Result Expected
+	void posTestIncorrectUserEmail() {
+		User u = new User(6, "kim@aol.com", "kim", "aol");
 		assertNotNull(u);
-		assertNotEquals(("aman@gmail.com"), u.getEmailId());
+		assertNotEquals(("aman@outlook.com"), u.getEmailId());
 	}
 
-	@Test
-	void testPositiveUserName() {
-		User u = new User(2, "xyz@gmail.com", "xyz", "xyz");
+	@Test // Positive Test Result Expected
+	void posTestCorrectUserName() {
+		User u = new User(4, "ar@aol.com", "ar", "aol");
 		assertNotNull(u);
-		assertEquals("xyz", u.getUserName());
+		assertEquals("ar", u.getUserName());
 	}
 
-	@Test
-	void testNegativeUserName() {
+	@Test // Positive Test Result Expected
+	void posTestIncorrectUserName() {
 		User u = new User(3, "test@aol.com", "test", "aol");
 		assertNotNull(u);
 		assertNotEquals("aol", u.getUserName());
+	}
+
+	@Test // Negative Test Result Expected
+	void negTestCorrectUserEmail() {
+		User u = new User(5, "michael@aol.com", "michael", "mike");
+		assertNotNull(u);
+		assertEquals("simon@gmail.com", u.getEmailId());
+	}
+
+	@Test // Negative Test Result Expected
+	void negTestIncorrectUserEmail() {
+		User u = new User(0, "shahzad@yahoo.com", "hzad", "shah");
+		assertNotNull(u);
+		assertNotEquals(("shahzad@yahoo.com"), u.getEmailId());
+	}
+
+	@Test // Negative Test Result Expected
+	void negTestCorrectUserName() {
+		User u = new User(6, "kim@aol.com", "kim", "aol");
+		assertNotNull(u);
+		assertEquals("aol", u.getUserName());
+	}
+
+	@Test // Negative Test Result Expected
+	void negTestIncorrectUserName() {
+		User u = new User(9, "abc@gmail.com", "abc", "cba");
+		assertNotNull(u);
+		assertNotEquals("abc", u.getUserName());
 	}
 
 }
